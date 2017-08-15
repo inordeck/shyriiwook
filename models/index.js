3c9af52d5fb9f4a14a2bcc786c4e7cc4eaf46bd6
inordeck <ian.nordeck@gmail.com>
2017-08-14 21:17:14 -0600

    working on heroku errors

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect(	process.env.MONGODB_URI || 
+					process.env.MONGOLAB_URI ||
+					process.env.MONGOHQ_URL ||
 					"mongodb://localhost/shyriiwook-db" );
 
 module.exports.user = require("./user.js");
